1a720613a75d76a05b7c9f8941be8f7921fdf9e7
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-10-08 17:03:39 +0300

    meme component imported

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Meme from './components/Meme';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Meme />
     </>
   );
 }
